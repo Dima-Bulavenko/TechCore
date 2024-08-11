d8fd8131c1c25faf61cfd053f682c9bbff35f315
Dima Bulavenko <dima99bylovenko@gmail.com>
2024-08-11 18:50:21 +0100

    feat: Create custom user model and manager and set the app to log in via email

--- tech_core/settings/base.py
+++ tech_core/settings/base.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local apps
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +130,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
