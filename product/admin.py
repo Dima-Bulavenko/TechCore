eaa4d49ec6e3caecd69d70fb82d29a34142c8c6a
Dima Bulavenko <dima99bylovenko@gmail.com>
2024-08-29 00:54:57 +0100

    feat: Register all models related to product on admin panel.

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from product.models import (
+    Attribute,
+    Category,
+    Manufacturer,
+    Product,
+    ProductAttributeValue,
+)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Attribute)
+class AttributeAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Manufacturer)
+class ManufacturerAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ProductAttributeValue)
+class ProductAttributeValueAdmin(admin.ModelAdmin):
+    pass
+
