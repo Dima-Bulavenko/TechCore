27e00f259a05a44a3a7d6296c1c0f03380c6dfe6
Dima Bulavenko <dima99bylovenko@gmail.com>
2024-09-01 23:24:24 +0100

    fixup! feat: Set up "creators.py" which contain classes that manage automatic filling database.

--- product/management/creators.py
+++ product/management/creators.py
@@ -15,22 +15,22 @@ class BaseCreator(ABC):
     def create(self):
         pass
 
-    @abstractmethod
     def validate(self):
-        pass
+        if not isinstance(self.data, dict):
+            raise CommandError("The data must be a dictionary")
+        
+        if not self.data.get("category"):
+            raise CommandError("Data must contain 'category' key")
+
+        if not self.data.get("attrs"):
+            raise CommandError("Data must contain 'attrs' key")
 
 
 class AttributesCreator(BaseCreator):
-    def __init__(self, data: dict, command_obj: BaseCommand):
-        self.command_obj = command_obj
-        self.data = data
-        self.validate()
-
     def create(self):
-        self.__set_category()
+        category = Category.objects.get(name=self.data["category"])
 
         for attr_name in self.data["attrs"]:
-            
             attribute_obj, created = Attribute.objects.get_or_create(name=attr_name.lower())
             if created:
                 msg = f"Attribute '{attr_name}' created successfully"
@@ -39,23 +39,4 @@ class AttributesCreator(BaseCreator):
                 msg = f"Attribute '{attr_name}' already exists"
                 self.command_obj.stdout.write(self.command_obj.style.WARNING(msg))
 
-            attribute_obj.category.add(self.category)
-                
-    def validate(self):
-        if not isinstance(self.data, dict):
-            raise CommandError("Top level of data must be key-value pairs")
-        
-        if not self.data.get("category"):
-            raise CommandError("Data must contain 'category' key")
-
-        if not self.data.get("attrs"):
-            raise CommandError("Data must contain 'attrs' key")
-    
-    def __set_category(self):
-        category_name = self.data["category"]
-
-        try:
-            self.category = Category.objects.get(name=category_name)
-        except Category.DoesNotExist as exc:
-            msg = f"Category '{category_name}' does not exist in the database"
-            raise CommandError(msg) from exc
+            attribute_obj.category.add(category)
