3b0d8fabfad7c23410dc09426767ccc242344837
Dima Bulavenko <dima99bylovenko@gmail.com>
2024-09-18 22:53:43 +0100

    feat: Create "Image" models to stores product photos and make migration for the model.

--- product/models.py
+++ product/models.py
@@ -30,6 +30,14 @@ class Attribute(models.Model):
             self.name = self.name.lower()
 
 
+class Image(models.Model):
+    product = models.ForeignKey('Product', on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='products/images')
+
+    def __str__(self):
+        return f"{self.product.name} image"
+
+
 class ProductAttributeValue(models.Model):
     product = models.ForeignKey('Product', on_delete=models.CASCADE)
     attribute = models.ForeignKey('Attribute', on_delete=models.CASCADE)
