eba4466813ee04d3ba969e62f88aa21edfc7e9a0
Dima Bulavenko <dima99bylovenko@gmail.com>
2024-09-19 22:04:24 +0100

    feat: Set django to manage media files on development server. Add media folder to ".gitignore".

--- tech_core/urls.py
+++ tech_core/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, re_path
 from django.views.defaults import page_not_found
@@ -25,3 +27,6 @@ urlpatterns = [
     path("__reload__/", include("django_browser_reload.urls")),
     path('', include('core.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
