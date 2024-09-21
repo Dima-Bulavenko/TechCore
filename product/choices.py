2911eef8b15a265f427ac89ad0df95d16d02ee1e
Dima Bulavenko <dima99bylovenko@gmail.com>
2024-09-21 22:07:38 +0100

    feat: Create "CPUAttributeChoices" class that store all attributes of a product of CPU category.

--- product/choices.py
+++ product/choices.py
@@ -17,4 +17,18 @@ class ManufacturerChoices(models.TextChoices):
     __empty__ = _('Select a manufacturer')
 
 
-__all__ = ["CategoryChoices", "ManufacturerChoices"]
+class CPUAttributeChoices(models.TextChoices):
+    PROCESSOR_ARCHITECTURE = "processor architecture", _("Processor Architecture")
+    CORE_COUNT = "core count", _("Core Count")
+    THREAD_COUNT = "thread count", _("Thread Count")
+    BASE_CLOCK_SPEED = "base clock speed", _("Base Clock Speed")
+    BOOST_CLOCK_SPEED = "boost clock speed", _("Boost Clock Speed")
+    CACHE_SIZE = "cache size", _("Cache Size")
+    TDP = "tdp (thermal design power)", _("TDP (Thermal Design Power)")
+    SOCKET_TYPE = "socket type", _("Socket Type")
+    LITHOGRAPHY = "lithography (process node)", _("Lithography (Process Node)")
+    INTEGRATED_GRAPHICS = "integrated graphics", _("Integrated Graphics")
+    UNLOCKED_FOR_OVERCLOCKING = "unlocked for overclocking", _("Unlocked for Overclocking")
+
+
+__all__ = ["CategoryChoices", "ManufacturerChoices", "CPUAttributeChoices"]
