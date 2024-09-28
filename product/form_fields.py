8776ecacea16ee24a8c480f7e6b7ab5cce9b7ae4
Dima Bulavenko <dima99bylovenko@gmail.com>
2024-09-28 01:32:20 +0100

    feat: Create "PriceRangeField" to show and validate form field for price filtering.

--- product/form_fields.py
+++ product/form_fields.py
@@ -1,5 +1,7 @@
 from django import forms
 
+from product import widgets
+
 
 class AttributeModelMultipleChoiceFiled(forms.ModelMultipleChoiceField):
     def label_from_instance(self, obj):
@@ -9,3 +11,23 @@ class AttributeModelMultipleChoiceFiled(forms.ModelMultipleChoiceField):
         if hasattr(value, "value"):
             return value.value
         return super().prepare_value(value)
+
+
+class PriceRangeField(forms.MultiValueField):
+    widget = widgets.PriceRangeWidget
+
+    def __init__(self, *args, **kwargs):
+        fields = (
+            forms.DecimalField(
+                min_value=0, max_value=999999, decimal_places=2, required=False
+            ),
+            forms.DecimalField(
+                min_value=0, max_value=999999, decimal_places=2, required=False
+            ),
+        )
+        super().__init__(fields, *args, **kwargs)
+
+    def compress(self, data_list):
+        if data_list:
+            return {"min_price": data_list[0], "max_price": data_list[1]}
+        return None
