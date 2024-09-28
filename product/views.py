5e71da4c516fef25d34aa6cdd61bf5187ffccb8e
Dima Bulavenko <dima99bylovenko@gmail.com>
2024-09-28 22:39:31 +0100

    feat: Create "ProductListView" to display particular products.
    The view create template for displaing specific products, return a queryset filltered
    products.

--- product/views.py
+++ product/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from product.forms import ProductFilterForm
+from product.services.filter import ProductFilter
+
+
+class ProductListView(ListView):
+    template_name = "core/index.html"
+    context_object_name = "products"
+    
+    def get_queryset(self):
+        return ProductFilter(category=self.kwargs.get("category"), data=self.request.GET).filter()
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = f"{self.kwargs.get('category')} Products"
+        context["filter_form"] = ProductFilterForm(product_category=self.kwargs.get("category"), data=self.request.GET)
+        return context
