f23ec19779e530729b025642eedb63ba18295dd4
Dima Bulavenko <dima99bylovenko@gmail.com>
2024-09-28 01:36:16 +0100

    feat: Create "ProductFilterForm" that allows to fillter different type of products in the same way.
    The form takes on required argument "product_category" that must corresponds to category's name of
    a particular product.

--- product/forms.py
+++ product/forms.py
@@ -1,5 +1,9 @@
 from django import forms
 
+from product import widgets
+from product.form_fields import AttributeModelMultipleChoiceFiled, PriceRangeField
+from product.models import Attribute, Manufacturer, product_category_mapper
+
 
 class ProductAttributeValueFormSet(forms.BaseInlineFormSet):
     def __init__(self, *args, **kwargs):
@@ -17,3 +21,41 @@ class ProductAttributeValueFormSet(forms.BaseInlineFormSet):
         if hasattr(self, "product_attributes"):
             return len(self.product_attributes)
         return super().total_form_count()
+
+
+class ProductFilterForm(forms.Form):
+    price_range = PriceRangeField()
+
+    def __init__(self, *args, product_category, **kwargs):
+        super().__init__(*args, **kwargs)
+        product_class = product_category_mapper.get_class(product_category)
+        self.set_manufacturer_field(product_class)
+        self.set_filter_attributes(product_class)
+
+    def set_filter_attributes(self, product_class):
+        for attr in product_class.get_filter_attributes():
+            attr_obj = Attribute.objects.get(name=attr)
+            attr_values = attr_obj.productattributevalue_set.filter(
+                product__category__name=product_class._category.value
+            )
+            self.fields[f"attr_{attr.value.replace(' ', '_')}"] = (
+                AttributeModelMultipleChoiceFiled(
+                    queryset=attr_values.values_list("value", named=True)
+                    .distinct()
+                    .order_by("value"),
+                    widget=widgets.CheckboxSelectMultiple,
+                    label=attr.label,
+                    required=False,
+                    to_field_name="value",
+                )
+            )
+
+    def set_manufacturer_field(self, product_class):
+        queryset = Manufacturer.objects.filter(
+            product__category__name=product_class._category.value
+        ).distinct()
+        self.fields["manufacturers"] = forms.ModelMultipleChoiceField(
+            queryset=queryset,
+            widget=widgets.CheckboxSelectMultiple,
+            required=False,
+        )
