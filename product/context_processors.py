2e7c0be3369a51940fa10e46d7b1cf823ae6f461
Dima Bulavenko <dima99bylovenko@gmail.com>
2024-10-04 19:58:08 +0100

    feat: Add "category_list" context processor to get categories in each template context.

--- product/context_processors.py
+++ product/context_processors.py
@@ -1,5 +1,10 @@
+from product.models import Category
 from product.services.order import ORDER_OPTIONS
 
 
 def order_options(request):  # noqa: ARG001
     return {'order_options': ORDER_OPTIONS}
+
+
+def category_list(request):
+    return {'categories': Category.objects.all()}
