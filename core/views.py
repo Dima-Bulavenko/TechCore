4fbb356c88e4d96e22bf1079e15fd072c250562e
Dima Bulavenko <dima99bylovenko@gmail.com>
2024-10-04 20:42:11 +0100

    feat: Add "set_order.js" to "index.html" template tag and make home page url redirect to CPU products page.

--- core/views.py
+++ core/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
 from django.views.generic import TemplateView
+
+from product import CategoryChoices
 
 
 class IndexView(TemplateView):
     template_name = 'core/index.html'
+    
+    def get(self, request, *args, **kwargs):
+        return redirect('product_list', category=CategoryChoices.CPU)
