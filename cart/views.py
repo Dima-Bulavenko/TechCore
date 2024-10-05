5e86f9cc996deb392f183d66ab52bdffd1e1ea4b
Dima Bulavenko <dima99bylovenko@gmail.com>
2024-10-05 17:03:54 +0100

    feat: Create view which handel item-addition to cart.

--- cart/views.py
+++ cart/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.http import HttpResponseNotFound
+from django.shortcuts import get_object_or_404, render
 from django.views.generic import View
+
+from cart.services.cart import Cart
+from product.forms import ProductQuantityForm
+from product.models import Product
 
 
 class CartActionView(View):
     def post(self, request, *args, **kwargs):
-        context = {"cart_count": 5}
-        return render(request, "components/shopping_card_button.html", context)
+        form = ProductQuantityForm(request.POST)
+        if not form.is_valid():
+            return HttpResponseNotFound()
+        
+        product_id = form.cleaned_data["product_id"]
+        quantity = form.cleaned_data["quantity"]
+        product = get_object_or_404(Product, pk=product_id)
+        cart = Cart(request)
+        cart.update(product, quantity)
+        return render(request, "components/shopping_card_button.html")
