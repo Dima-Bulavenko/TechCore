a27c2e6c9d9323bd67e8cffabd286d2823920554
Dima Bulavenko <dima99bylovenko@gmail.com>
2024-10-10 23:24:47 +0100

    feat: Create "Address" model to store user's addresses.

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,30 @@
+import uuid
+
+from django.conf import settings
 from django.db import models
+from django.db.models import Sum
+from django.utils.translation import gettext_lazy as _
+
+from cart.services.cart import get_delivery_cost
+from product.models import Product
+
+
+class Address(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, 
+                             on_delete=models.CASCADE, 
+                             related_name="addresses", 
+                             blank=True, null=True)
+    address_line_1 = models.CharField(_("Address Line 1"), max_length=100)
+    address_line_2 = models.CharField(_("Address Line 2"), max_length=100, blank=True)
+    city = models.CharField(_("City"), max_length=100)
+    county = models.CharField(_("County"), max_length=100)
+    country = models.CharField(_("Country"), max_length=100)
+    postal_code = models.CharField(_("Postal Code"), max_length=20, blank=True)
+    phone_number = models.CharField(_("Phone Number"), max_length=20)
+    last_update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.address_line_1}"
+
 
 # Create your models here.
--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@
 from typing import ClassVar
 
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 from django.db.models import CharField, EmailField, ImageField
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
@@ -34,3 +35,18 @@ class User(AbstractUser):
 
         """
         return reverse("users:detail", kwargs={"pk": self.id})
+
+
+class Address(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="addresses", blank=True, null=True)
+    address_line_1 = models.CharField(_("Address Line 1"), max_length=100)
+    address_line_2 = models.CharField(_("Address Line 2"), max_length=100, blank=True)
+    city = CharField(_("City"), max_length=100)
+    county = CharField(_("County"), max_length=100)
+    country = CharField(_("Country"), max_length=100)
+    postal_code = CharField(_("Postal Code"), max_length=20, blank=True)
+    phone_number = CharField(_("Phone Number"), max_length=20)
+    last_update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.address_line_1}"
