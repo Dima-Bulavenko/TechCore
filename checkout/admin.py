c969691fda0782276c3ec2054180ef373938871a
Dima Bulavenko <dima99bylovenko@gmail.com>
2024-10-13 16:56:19 +0100

    feat: Add custom user model to django admin.

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from checkout import models
+
+
+@admin.register(models.Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = [
+        'user',
+        'address_line_1',
+        'address_line_2',
+        'city',
+        'county',
+        'country',
+        'postal_code',
+        'last_update',
+    ]
