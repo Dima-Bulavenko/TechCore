c376100d4456d2f984a1c86ab6123715edcefcae
Dima Bulavenko <dima99bylovenko@gmail.com>
2024-10-19 03:17:51 +0100

    feat: Create "post" method of "CheckoutView" view to proccess post request for user payment.
    the method validate user address and order details, proccess case
    when payment needs additional acction to authenticate, confirm payment,
    return user on success page.

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import redirect
-from django.views.generic import TemplateView
+import json
 
-from checkout.models import Order
+from django.contrib import messages
+from django.shortcuts import redirect, resolve_url
+from django.views.generic import TemplateView
+from django_htmx.http import HttpResponseClientRedirect
+
+from cart.services.cart import Cart
 from checkout.services.checkout import Checkout
 from checkout.services.stripe import Stripe
 
@@ -15,22 +19,72 @@ class CheckoutView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
         stripe = Stripe(self.checkout.cart)
-        stripe_intent = stripe.intent()
         context["cart"] = self.checkout.cart
-        context.update(self.checkout.get_forms())
         context["stripe_public_key"] = stripe.pubic_key
-        context["client_secret"] = stripe_intent.client_secret
+        context.update(self.checkout.get_forms())
         return context
 
-    def post(self, request, *args, **kwargs):
-        order = self.checkout.create_order()
-
-        if order:
-            self.checkout.cart.clear()
-            return redirect("checkout_success", order.order_number)
+    def get(self, request, *args, **kwargs):
+        if not self.checkout.cart:
+            return redirect("cart")
         return super().get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        status_params = {}
+        intent = None
+        if "payment_intent_id" in request.POST:
+            stripe = Stripe(self.checkout.cart).stripe
+            intent = stripe.PaymentIntent.confirm(
+                request.POST["payment_intent_id"],
+                return_url=request.build_absolute_uri(resolve_url("home"))
+            )
+        else:
+            forms = self.checkout.get_forms()
+            if all(form.is_valid() for form in forms.values()):
+                status_params.update(isFormValid=True)
+                address_form_data = forms["address_form"].cleaned_data
+                order_form_data = forms["order_form"].cleaned_data
+                stripe = Stripe(self.checkout.cart)
+                return_url = request.build_absolute_uri(resolve_url("home"))
+                intent_params = {
+                    "payment_method": request.POST.get("payment_method_id"),
+                    "confirm": True,
+                    "confirmation_method": "manual",
+                    "return_url": return_url,
+                    "use_stripe_sdk": True,
+                    "metadata": {
+                        "cart": json.dumps(self.checkout.cart.cart),
+                        "order": json.dumps(order_form_data),
+                        "address": json.dumps(address_form_data),
+                        "user_email": self.request.user.email
+                        if self.request.user.is_authenticated
+                        else None,
+                    },
+                }
+
+                try:
+                    intent = stripe.intent(**intent_params)
+                    print(intent.client_secret)
+                except stripe.stripe.error.CardError as e:
+                    messages.error(request, "Your payment was declined")
+                    status_params.update(cardError=e.user_message)
+            else:
+                status_params.update(isFormValid=False)
+        if intent:        
+            if intent.status == "requires_action":
+                status_params.update(clientSecret=intent.client_secret)
+                status_params.update(requiredAction=True)
+            elif intent.status == "succeeded":
+                messages.success(request, "Your order was successful")
+                self.checkout.cart.clear()
+                success_url = resolve_url("checkout_success", intent_id=intent.id)
+                return HttpResponseClientRedirect(success_url)
+            else:
+                status_params.update(cardError="Invalid PaymentIntent status")
+        context.update(status_params=status_params)
+        return self.render_to_response(context)
 
 
 class CheckoutSuccessView(TemplateView):
