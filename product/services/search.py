716e717f295071290e096e1c440ad1a0fe97d7bf
Dima Bulavenko <dima99bylovenko@gmail.com>
2024-10-30 05:49:27 +0000

    fix: Fix "Search input error" close #42.

--- product/services/search.py
+++ product/services/search.py
@@ -1,6 +1,8 @@
-from django.contrib.postgres.search import SearchQuery, SearchVector
+from django.conf import settings
+from django.db.models import Avg, CharField, OuterRef, Q, Subquery, Value
+from django.db.models.functions import Concat
 
-from product.models import Product
+from product.models import Image, Product
 
 
 class ProductSearch:
@@ -8,11 +10,24 @@ class ProductSearch:
         self.query = query
 
     def search(self):
-        search_vector = SearchVector(
-            "manufacturer__name", 
-            "name",
-            "category__name",
-            "description",
+        # TODO Refactor, this code was taken from ProductFilter
+        queryset = Product.objects.prefetch_related(
+            "productattributevalue_set__attribute",
+            "reviews",
+        ).filter(
+            Q(name__icontains=self.query) | 
+            Q(description__icontains=self.query) |
+            Q(manufacturer__name__icontains=self.query)
         )
-        search_query = SearchQuery(self.query)
-        return Product.objects.annotate(search=search_vector).filter(search=search_query)
+
+        images = Image.objects.filter(product=OuterRef("pk"))
+
+        queryset = queryset.annotate(
+            image_url=Concat(
+                Value(settings.MEDIA_URL),
+                Subquery(images.values("image")[:1]),
+                output_field=CharField(),
+            ),
+            rating_avg=Avg("reviews__rating", default=0),
+        )
+        return queryset
